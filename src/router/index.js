91876657519d415b878198da388e362bb5b68c92
Mou <madalmou.2004@gmail.com>
2024-01-07 21:34:36 +0530

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
-// import Contact from '../views/Contact.vue'
-// import About from '../views/About.vue'
 import Login from '../views/Login.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend_vue_project',
   plugins: [
     vue(),
   ],
