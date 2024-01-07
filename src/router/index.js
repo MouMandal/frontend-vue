bc95274d1f159cd5963a01c06c307ad46ee54bab
Mou <madalmou.2004@gmail.com>
2024-01-07 21:56:57 +0530

    my commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/Home.vue'
+// import Contact from '../views/Contact.vue'
+// import About from '../views/About.vue'
 import Login from '../views/Login.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
